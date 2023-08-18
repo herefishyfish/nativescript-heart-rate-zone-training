94ca15d828dce0dbe23c9c2658be93e797b98b3a
herefishyfish <46717769+herefishyfish@users.noreply.github.com>
2023-08-18 22:51:57 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nativescript-heart-rate-zone-training
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nativescript-stackblitz-templates-d42xbd)
---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ns-app',
+  template: `
+    <Label>Hello</Label>
+  `,
+})
+export class AppComponent {}
